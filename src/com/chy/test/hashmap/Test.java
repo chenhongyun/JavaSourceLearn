fb64da8365cbd37503f1f01069962fc93de55c79
chenhongyun <714908876@qq.com>
2020-04-16 16:21:48 +0800

    init

---
+++ src/com/chy/test/hashmap/Test.java
@@ -0,0 +1,17 @@
+package com.chy.test.hashmap;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Test {
+    public static void main(String[] args) {
+
+        Map<String, Double> map = new HashMap<>();
+        map.put("K1", 0.1);
+        map.put("K2", 0.2);
+        map.put("K3", 0.3);
+        map.put("K4", 0.4);
+        System.out.println("结束");
+    }
+
+}
